750eb84b634922a0f5fbff1f80b28ccf2a545b29
zhaoqi <kris.zhao@yoho.cn>
2016-06-27 17:44:00 +0800

    initial

---
+++ src/main/java/com/zhaoqi/component/filter/DotaCORFilter.java
@@ -0,0 +1,38 @@
+package com.zhaoqi.component.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by zhaoqi on 2016/5/5.
+ * 过滤器，解决浏览器跨域
+ */
+public class DotaCORFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, accept, Origin");
+        response.setHeader("Content-Type", "application/json");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
